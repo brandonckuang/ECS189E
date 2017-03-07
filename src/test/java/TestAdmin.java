23cf4be9ee64dc6ec34af094909089119be095e9
bckuang <brandonckuang@gmail.com>
2017-03-07 01:06:15 -0800

    New Tests

--- src/test/java/TestAdmin.java
+++ src/test/java/TestAdmin.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.*;
 
 public class TestAdmin {
 
+    //still need to test for uniqueness of a class/year combination. (key?)
+
     private IAdmin admin;
 
     @Before
@@ -24,15 +26,16 @@ public class TestAdmin {
     //Tests whether the class exists, which was defined in setup
     @Test
     public void testMakeClass2() {
-        this.admin.createClass("ECS120", 2017, "Rogaway", 15);
-        assertTrue(this.admin.classExists("ECS120", 2017));
+        this.admin.createClass("ECS120", 2018, "Rogaway", 15);
+        assertTrue(this.admin.classExists("ECS120", 2018));
     }
 
-    //Creates a test in the function and tests for its existence
+    //Creates a class for the past and should assertFalse since it is not calendar year
+    //Fails
     @Test
     public void testMakeClass3() {
         this.admin.createClass("ECS129", 2016, "Koehl", 15);
-        assertTrue(this.admin.classExists("ECS129", 2016));
+        assertFalse(this.admin.classExists("ECS129", 2016));
     }
 
     //Tests for a Course that does not exist
@@ -41,30 +44,97 @@ public class TestAdmin {
         assertFalse(this.admin.classExists("Fail", 2016));
     }
 
+    //creating class with zero/negative capacity
+    //Fails
+    @Test
+    public void testMakeClass5() {
+        this.admin.createClass("ECS15", 2017, "Instructor6", 0);
+        if((this.admin.getClassCapacity("ECS15", 2017)) <= 0) {
+            fail("Zero/negative class capacity");
+        }
+    }
+
+    //Testing for more than 2 courses per professor
+    //Fails
+    @Test
+    public void testMakeClass6() {
+        this.admin.createClass("ECS1", 2017, "Instructor10", 15);
+        this.admin.createClass("ECS2", 2017, "Instructor10", 15);
+        this.admin.createClass("ECS3", 2017, "Instructor10", 15);
+        assertFalse("Professor already teaching two courses",this.admin.classExists("ECS3", 2017));
+
+    }
+
+    //Testing for three classes for the same professor but one is in a different year and should assertTrue
+    @Test
+    public void testMakeClass7() {
+        this.admin.createClass("ECS4", 2017, "Instructor", 15);
+        this.admin.createClass("ECS5", 2017, "Instructor", 15);
+        this.admin.createClass("ECS6", 2018, "Instructor", 15);
+        assertTrue(this.admin.classExists("ECS6", 2018));
+
+    }
+
+    //Tests for empty string for instructor, which should assertFalse
+    //Fails
+    @Test
+    public void testMakeClass8() {
+        this.admin.createClass("ECS8", 2017, "", 15);
+        assertFalse(this.admin.classExists("ECS8", 2017));
+    }
+
+    //Tests to see if there is a classname
+    //Fails
+    @Test
+    public void testMakeClass9() {
+        this.admin.createClass("", 2017, "Instructor9", 15);
+        assertFalse(this.admin.classExists("", 2017));
+    }
+
+    //Test for different letter cases
+    @Test
+    public void testMakeClass10() {
+        this.admin.createClass("ECS200", 2017, "Instructor200", 15);
+        this.admin.createClass("ecs200", 2017, "Instructor200", 15);
+        assertTrue(this.admin.classExists("ECS200", 2017));
+        assertFalse(this.admin.classExists("ecs200", 2017));
+
+    }
+
+    //Tests for the creation of a class for year 0
+    //Fail
+    @Test
+    public void testMakeClass11() {
+        this.admin.createClass("ECS1000", 0, "Instructor1000", 15);
+        assertFalse(this.admin.classExists("ECS1000", 0));
+    }
+
+
     //Tests whether a professor teaches a class, which should be true
+
     @Test
     public void testGetClassInstructor() {
-        this.admin.createClass("ECS127", 2017, "Rogaway", 15);
-        assertTrue(this.admin.getClassInstructor("ECS127", 2017).equals("Rogaway"));
+        this.admin.createClass("ECS12", 2017, "Instructor15", 15);
+        assertTrue(this.admin.getClassInstructor("ECS12", 2017).equals("Instructor15"));
     }
 
     //Tests for a false professor teaching a class.
     @Test
     public void testGetClassInstructor2() {
-        this.admin.createClass("ECS127", 2017, "Rogaway", 15);
-        assertFalse(this.admin.getClassInstructor("ECS127", 2017).equals("False"));
+        this.admin.createClass("ECS10", 2018, "Rogaway", 15);
+        assertFalse(this.admin.getClassInstructor("ECS10", 2018).equals("False"));
     }
 
     //Tests for the null condition when a nonexistent class is given as parameters
     @Test
     public void testGetClassInstructor3() {
-        assertNull(this.admin.getClassInstructor("False", 0));
+        assertNull(this.admin.getClassInstructor("SoFalse", 0));
     }
 
 
     @Test
     public void testGetCapacity(){
-        this.admin.createClass("Test", 2017, "Instructor", 15);
+        this.admin.createClass("Test", 2017, "Instructor2", 15);
         assertEquals(15, (double)(this.admin.getClassCapacity("Test", 2017)), 0);
 
     }
@@ -74,16 +144,43 @@ public class TestAdmin {
         assertEquals(-1, (double)(this.admin.getClassCapacity("fail", 2016)), 0);
     }
 
+    //Testing Change Capacity to make sure it does change the capacity
     @Test
     public void testChangeCapacity() {
-        this.admin.createClass("ECS127", 2017, "Rogaway", 15);
-        this.admin.changeCapacity("ECS127", 2017, 16);
-        assertEquals(16, (double)(this.admin.getClassCapacity("ECS127", 2017)), 0);
+        this.admin.createClass("ECS13", 2017, "Instructor4", 15);
+        this.admin.changeCapacity("ECS13", 2017, 16);
+        //ssertEquals(16, (double)(this.admin.getClassCapacity("ECS13", 2017)), 0);
+        assertTrue(this.admin.getClassCapacity("ECS13", 2017) == 16);
+    }
+
+    //changed the capacity to 0 but expected that it will do nothing or should not change the capacity
+    //Fails
+    @Test
+    public void testChangeCapacity2() {
+        this.admin.createClass("ECS14", 2017, "Instructor5", 15);
+        this.admin.changeCapacity("ECS14", 2017, 0);
+        //assertEquals(15, (double)(this.admin.getClassCapacity("ECS14", 2017)), 0);
+        assertTrue(this.admin.getClassCapacity("ECS14", 2017) == 15);
     }
 
     @Test
-    public void testChangeCapacity2() {
-        this.admin.createClass("ECS127", 2017, "Rogaway", 15);
-        this.admin.changeCapacity("False", 2017, 0);
+    public void testChangeCapacity3() {
+        this.admin.createClass("ECS16", 2017, "Instructor15", 15);
+        this.admin.changeCapacity("ECS16", 2017, 15);
+      //  assertEquals(15, (double)(this.admin.getClassCapacity("ECS16", 2017)), 0);
+        assertTrue(this.admin.getClassCapacity("ECS16", 2017) == 15);
     }
+
+    //Tests for duplicate classes being added
+    @Test
+    public void testUniqueClass() {
+        this.admin.createClass("ECS123", 2017, "me", 15);
+        this.admin.createClass("ECS123", 2017, "you", 15);
+        if((this.admin.getClassInstructor("ECS123", 2017)).equals("you")) {
+            fail("Duplicate class created");
+        }
+        else assertTrue(true);
+    }
+
 }
+
--- src/test/java/TestInstructor.java
+++ src/test/java/TestInstructor.java
@@ -1,7 +1,9 @@
 import api.IAdmin;
 import api.IInstructor;
+import api.IStudent;
 import api.core.impl.Admin;
 import api.core.impl.Instructor;
+import api.core.impl.Student;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,15 +15,61 @@ import static org.junit.Assert.*;
 public class TestInstructor {
 
     private IInstructor instructor;
+    private IAdmin admin;
+    private IStudent student;
 
     @Before
     public void setup() {
         this.instructor = new Instructor();
+        this.admin = new Admin();
+        this.student = new Student();
+
     }
 
+    //Creating homework and seeing if it exists
+    //Should return true
     @Test
-    public void testMakeClass() {
-        //this.instructor.()
+    public void testAddHomework() {
+        this.admin.createClass("ECS1", 2017, "Instructor1", 15);
+        this.instructor.addHomework("Instructor1", "ECS1", 2017, "HW1",
+                "homework1 description");
+        assertTrue(this.instructor.homeworkExists("ECS1", 2017, "HW1"));
+    }
+
+    //Testing to see if instructor is assigned to the class
+    //Test should fail since instructor assigned is Instructor 2 but Instructor 1 can add homework
+    @Test
+    public void testAddHomework2() {
+        this.admin.createClass("ECS2", 2017, "Instructor2", 15);
+        this.instructor.addHomework("Instructor1", "ECS2", 2017, "HW2",
+                "homework2 description");
+        assertFalse(this.instructor.homeworkExists("ECS2", 2017, "HW2"));
+    }
+
+    //Finding homework for a class that does not currently exist
+    //Test should pass
+    @Test
+    public void testAddHomework3() {
+        this.admin.createClass("ECS3", 2017, "Instructor3", 15);
+        this.instructor.addHomework("Instructor3", "ECS3", 2017, "HW3" ,
+                "homework 3 description");
+        assertFalse(this.instructor.homeworkExists("ECS4", 2017, "HW3"));
+    }
+
+    //Testing to see if homework exists. Class will exist but the homework does not
+    //Test should pass
+    @Test
+    public void testAddHomework4() {
+        this.admin.createClass("ECS4", 2017, "Instructor4", 15);
+        this.instructor.addHomework("Instructor4", "ECS4", 2017, "HW4",
+                "homework 4 description");
+        assertFalse(this.instructor.homeworkExists("ECS4", 2017, "HW5"));
+    }
+
+
+    @Test
+    public void testAddHomework5() {
 
     }
 }
+
